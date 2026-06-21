2d5bffecea0e3e3f41723fbfe64733e1ea36c66f
rEaLlY <chigoexploitkid@atomicmail.io>
2026-06-20 23:40:40 -0400

    Add hardware.h header file with TPM write function

---
+++ vnv-switcher/native/include/hardware.h
@@ -0,0 +1,14 @@
+#ifndef HARDWARE_H
+#define HARDWARE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int execute_tpm_write(int target_version);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // HARDWARE_H
